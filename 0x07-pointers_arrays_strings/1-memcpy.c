230c1c739bfa464634126576f6648aa49d1d2c1c
Fasil11 <seifufasil@gmail.com>
2022-11-01 03:57:24 -0700

    Task1

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -13,8 +13,7 @@ char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int index;
 	unsigned char *destination = dest;
-
-	const unsisgned char *source = src;
+	const unsigned char *source = src;
 
 	for (index = 0; index < n; index++)
 		destination[index] = source[index];
