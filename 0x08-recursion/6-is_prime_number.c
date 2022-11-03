efa568c361f1c2789dd9962ba4c9fe4e2b2e727d
Fasil11 <seifufasil@gmail.com>
2022-11-03 05:25:31 -0700

    task6

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,30 +1,33 @@
 #include "main.h"
 
 /**
- * sqrt2 - makes possible to evaluate from 1 to n
+ * prime2 - Makes possible to evaluate from 1 to n
  * @a: same number as n
- * @b: number that itrates from 1 to n
+ * @b: number that iterates from 1 to n
  *
- * Return: On success 1.
+ * Return: On success 1
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int prime2(int a, int b)
+{
+	if (a == b)
+		return (1);
+	else if (a % b == 0)
+		return (0);
+	return (prime2(a, b + 1));
+}
+/**
+ * is_prime_number - checks if a number is prime
+ * @n: Number Integer
+ *
+ * Return: On success 1
  * On error, -1 is returned, and errno is set appropriately.
  */
 
-int sqrt2(int a, int b)
+int is_prime_number(int n)
+
 {
-	if (b * b == a)
-		return (b);
-	else if (b * b > a)
-		return (-1);
-	return (sqrt2(a, b + 1));
-}
-/**
- * _sqrt_recursion - returns the natural square root of n
- * @n: Number Integer
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
-int _sqrt_recursion(int n)
-{
-	return (sqrt2(n, 1));
+	if (n <= 1)
+		return (0);
+	return (prime2(n, 2));
 }
