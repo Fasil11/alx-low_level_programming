11dc140e715e1b977f3d4c2c370bb0230ffc47a6
Fasil11 <seifufasil@gmail.com>
2023-01-16 12:45:18 -0800

    task0

---
+++ 0x18-dynamic_libraries/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _abs - Computes the absolute value of an intiger
+ * @t: The integr to be computed
+ * Return: The absoultee value
+ */
+int _abs(int t)
+{
+	if (t >= 0)
+		return (t);
+	else
+		return (-t);
+}
