8fc6cca2a2870d6d7154cb96bb98f9c48ead978e
Fasil11 <seifufasil@gmail.com>
2023-05-09 12:09:38 -0700

    tasks

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -16,16 +16,17 @@
  *
  */
 
-int linear_search(int *array, size_t size, int value) {
-    if (array == NULL) {
-        return -1;
-    }
-    for (int i = 0; i < size; i++) {
-        if (array[i] == value) {
-            printf("Value %d found at index %d\n", value, i);
-            return i;
-        }
-    }
-    printf("Value %d not found in array\n", value);
-    return -1;
+int linear_search(int *array, size_t size, int value)
+{
+	size_t i = 0;
+
+	if (array == NULL)
+		return (-1);
+
+	for (; i < size; i++)
+	{
+		printf("Value checked array[%lu] = [%d]\n", i, array[i]);
+		if (array[i] == value)
+			return (i);
+	} return (-1);
 }
