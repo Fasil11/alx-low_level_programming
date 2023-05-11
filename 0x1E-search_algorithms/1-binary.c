62f0ee33087fc34c69668ffe53c2b5f603a09dde
Fasil11 <seifufasil@gmail.com>
2023-05-11 03:37:49 -0700

    tasks

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -10,27 +10,30 @@
  */
 int binary_search(int *array, size_t size, int value)
 {
-	size_t i = 0;
-	int *a = array;
+    size_t i = 0;
+    int *a = array;
 
-	if (!array)
-		return (-1);
+    if (!array)
+        return (-1);
 
-	while (size)
-	{
-		for (i = 0, printf("Searching in array: "); i < size; i++)
-			printf("%d%s", a[i], i + 1 == size ? "\n" : ", ");
+    while (size)
+    {
+        for (i = 0, printf("Searching in array: "); i < size; i++)
+            printf("%d%s", a[i], i + 1 == size ? "\n" : ", ");
+	
+        i = (size - 1) / 2;
 
-		i = (size - 1) / 2;
-		if (a[i] == value)
-			return ((a - array) + i);
-		else if (a[i] > value)
-			size = i;
-		else
-		{
-			a += (i + 1);
-			size -= (i + 1);
-		}
-	}
-	return (-1);
+        if (a[i] == value)
+            return ((a - array) + i);
+
+ 	else if (a[i] > value)
+            size = i;
+
+     	else
+        {
+            a += (i + 1);
+            size -= (i + 1);
+        }
+    }
+    return (-1);
 }
