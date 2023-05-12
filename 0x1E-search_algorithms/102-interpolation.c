9ffd736062351cea4d3d65c9691c20493611e1af
Fasil11 <seifufasil@gmail.com>
2023-05-12 15:27:17 -0700

    tasks

--- 0x1E-search_algorithms/102-interpolation.c
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -8,29 +8,44 @@
  *
  * Return: the index found or -1
  */
+
 int interpolation_search(int *array, size_t size, int value)
 {
-	size_t pos = 0, low = 0, high = size - 1;
+	size_t low = 0, high, mid;
 
-	if (!array || !size)
+	if (array == NULL)
 		return (-1);
 
-	while (1)
+	high = size - 1;
+	mid = low + (((double)(high - low) / (array[high] - array[low]))
+		     * (value - array[low]));
+
+	while (array[high] != array[low] && value >= array[low] && value <=
+	       array[high])
 	{
-		pos = low + (((double)(high - low) /
-			  (array[high] - array[low])) * (value - array[low]));
-		if (pos >= size)
-		{
-			printf("Value checked array[%lu] is out of range\n", pos);
-			break;
-		}
-		printf("Value checked array[%lu] = [%d]\n", pos, array[pos]);
-		if (array[pos] == value)
-			return (pos);
-		else if (array[pos] > value)
-			high = pos - 1;
+		mid = low + (((double)(high - low) / (array[high] - array[low]))
+			     * (value - array[low]));
+
+		printf("Value checked array[%d] = [%d]\n", (int) mid,
+		       array[mid]);
+
+		if (value == array[mid - 1])
+			return ((int) mid - 1);
+
+		else if (value == array[mid])
+			return ((int) mid);
+
+		else if (value < array[mid])
+			high = mid - 1;
+
 		else
-			low = pos + 1;
+			low = mid + 1;
 	}
+
+	if (value == array[low])
+		return ((int) low);
+
+	printf("Value checked array[%d] is out of range\n", (int) mid);
+
 	return (-1);
 }
